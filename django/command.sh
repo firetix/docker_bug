650a74999a820b06c8db4ca7aaf804f089820e5d
Mohamed Rachidi <rachidi29@gmail.com>
2016-04-21 18:43:15 +0100

    minr change

--- django/command.sh
+++ django/command.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
 python /home/docker/django/bootstrap.py
-mkdir -p /var/log/backend/supervisor/
-mkdir -p /var/log/supervisor/
-mkdir -p /tmp/backend/
-/usr/local/bin/uwsgi --ini /home/docker/django/conf/uwsgi.ini
-/usr/local/bin/supervisord
